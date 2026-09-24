c0ecf9a3e4535e51424de9950ae0f07a0b62d8c4
jaevit17 <jaevit17@gmail.com>
2026-09-23 21:29:48 -0500

    Funcion: calcular creada, Muestra en Pantalla valor: Disponible

--- funciones.js
+++ funciones.js
@@ -1,4 +1,7 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+/*Calculo del sobrante disponible despues 
+  de restar Egresos de los Ingresos*/
 function calcularDisponible(ingresos,egresos){
     let disponible=ingresos-egresos;
     if(disponible<0){
--- simulador.js
+++ simulador.js
@@ -1,1 +1,18 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    //Recupera Ingresos y muestra en Float
+    let cmpIngresos=document.getElementById("txtIngresos");
+    let ingresosText=cmpIngresos.value;
+    let ingresos=parseFloat(ingresosText);
+    //Recupera Egresos y muestra en Float
+    let cmpEgresos=document.getElementById("txtEgresos");
+    let egresosText=cmpEgresos.value;
+    let egresos=parseFloat(egresosText);
+    //LLamar a calcularDisponible
+    let resultDisponible=calcularDisponible(ingresos,egresos);
+    /*Mostrar en pantalla valor Disponible
+      Redondeo a 2 decimales cumpliendo el Caso de Prueba 3 */
+    let cmpDisponible=document.getElementById("spnDisponible");
+    cmpDisponible.textContent=resultDisponible.toFixed(2);
+    
+}
